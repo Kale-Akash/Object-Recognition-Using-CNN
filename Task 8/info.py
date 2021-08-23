6897a0f8bfd3c37dafcc6c609db2cea2d32609ba
Akash Kale <74634368+Kale-Akash@users.noreply.github.com>
2021-08-23 13:55:45 +0530

    Add files via upload

---
+++ Task 8/info.py
@@ -0,0 +1,14 @@
+print(
+'''
+Registration Name: XYZ
+Car Name: Audi
+Ownership: Second Owner
+Registration Date: 19th Jun 2012
+Car Number: MH13 BN 8454
+Engine NO: CHM0XXXXX
+Market Model: AUDI AG, AUDI A4 1.0 TDI
+Fuel: DIESEL
+RC Status: ACTIVE
+Vehicle Age: 9years
+Vehicle Class: Motor Car (LMW)
+''')
